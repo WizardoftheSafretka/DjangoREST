4258d3113f49e7f76e5faf2a90c05a57ec8b9aff
Wizard <merihov2yandex.ru>
2026-04-28 18:57:35 +0300

    finish working

---
+++ materials/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='Укажите название курса')
+    review = models.ImageField(upload_to='materials/photos', blank=True, null=True, verbose_name='Первью',
+                                help_text='Загрузите фото')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание', help_text='Укажите описание курса')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='Укажите название урока')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс',
+                               help_text='Укажите название курса', related_name='lessons')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание', help_text='Укажите описание урока')
+    review = models.ImageField(upload_to='materials/photos', blank=True, null=True, verbose_name='Первью',
+                                help_text='Загрузите фото')
+    url = models.TextField(blank=True, null=True, verbose_name='Ссылка на видео',
+                           help_text='Укажите ссылку на видео с уроком')  # Добавлена закрывающая скобка и запятая
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def __str__(self):
+        return f"{self.title} (курс: {self.course.title})"
---
+++ users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+
+class User(AbstractBaseUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите фото",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
