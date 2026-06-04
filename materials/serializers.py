47a30d07854f17b5f2ffaf4e3932bc0f8b3b0c8d
Wizard <merihov2yandex.ru>
2026-06-04 20:23:40 +0300

    finish 2

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,43 +1,15 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-
 from materials.models import Course, Lesson, Subscribe
 from materials.validators import validate_forbidden_word
 
 
 class CourseBaseSerializer(ModelSerializer):
-    title = serializers.CharField(validators=[validate_forbidden_word])
+    url = serializers.CharField(validators=[validate_forbidden_word])
 
     class Meta:
         model = Course
         fields = ['id', 'title', 'review', 'description']
-
-
-class LessonSerializer(ModelSerializer):
-    title = serializers.CharField(validators=[validate_forbidden_word])
-    course = CourseBaseSerializer(read_only=True)
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
-        read_only_fields = ('owner',)
-
-
-class CourseSerializer(ModelSerializer):
-    title = serializers.CharField(validators=[validate_forbidden_word])
-    lessons_count = SerializerMethodField()
-    lessons = SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'review', 'description', 'lessons_count', 'lessons']
-
-    def get_lessons_count(self, obj):
-        return obj.lessons.count()
-
-    def get_lessons(self, obj):
-        lessons = obj.lessons.all()
-        return LessonSerializer(lessons, many=True).data  # ✅ Теперь это безопасно
 
 
 class SubscribeSerializer(ModelSerializer):
@@ -47,3 +19,44 @@ class SubscribeSerializer(ModelSerializer):
     class Meta:
         model = Subscribe
         fields = ['id', 'course', 'user', 'is_subscribe']
+
+
+class LessonSerializer(ModelSerializer):
+    url = serializers.CharField(validators=[validate_forbidden_word])
+    course = CourseBaseSerializer(read_only=True)
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+        read_only_fields = ('owner',)
+
+
+class CourseSerializer(ModelSerializer):
+    url = serializers.CharField(validators=[validate_forbidden_word])
+    lessons_count = SerializerMethodField()
+    lessons = SerializerMethodField()
+    is_subscribed = SerializerMethodField()
+    owner = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'review', 'description', 'lessons_count',
+                  'lessons', 'is_subscribed', 'owner']
+        read_only_fields = ('owner',)
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    def get_lessons(self, obj):
+        lessons = obj.lessons.all()
+        return LessonSerializer(lessons, many=True).data
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return Subscribe.objects.filter(
+                course=obj,
+                user=request.user,
+                is_subscribe=True
+            ).exists()
+        return False
--- materials/validators.py
+++ materials/validators.py
@@ -3,5 +3,5 @@ from rest_framework.exceptions import ValidationError
 forbidden_word = "youtube"
 
 def validate_forbidden_word(value):
-	if forbidden_word in value.lower():
+	if forbidden_word not in value.lower():
 		raise ValidationError(value)
